c347c684932f2305232dee5e7049b8e57d73f8f7
Tvarog <m_tvarog@utb.cz>
2022-05-21 18:24:02 +0200

    program

--- ProjektAK2TP/Program.cs
+++ ProjektAK2TP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ProjektAK2TP;
 
-Console.WriteLine($"Control number is: {GetControlNumberForCode.GetControlNumberForCodeMethod()}");
-
+// Console.WriteLine($"Control number for barcode is: {GetControlNumberForCode.GetControlNumberForBarcode()}");
+Console.WriteLine($"Control number for ssn is: {GetControlNumberForCode.GetControlNumberForSsn()}");
+GetControlNumberForCode.CheckControlNumberForSsn();
 
